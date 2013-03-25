281ff364dc88998cc5c0c804f43426a2b452a980
gjordana <gjordana@gjordana>
2013-03-25 19:32:46 +0100

    ara si modificats be

--- apliWeb/escola/models.py
+++ apliWeb/escola/models.py
@@ -1,3 +1,37 @@
+# -*- coding: utf-8 -*-
 from django.db import models
 
 # Create your models here.
+
+class Escola(models.Model):
+	equip = models.CharField(max_length=200)
+	junta = models.CharField(max_length=200)
+	
+	def __unicode__ (self):
+		return self.equip
+	
+
+class Instalacio(models.Model):
+	vestidor = models.TextField(max_length=200)
+	camp = models.TextField(max_length=200)
+	
+	def __unicode__(self):
+		return self.vestidor
+	
+
+class Reglament(models.Model):
+	bases = models.TextField(max_length=2000)
+	
+	def __unicode__(self):
+		return self.base
+
+class Calendari(models.Model):
+	#data = 
+	partit = models.CharField(max_length=200)
+	#hora =
+
+	def __unicode__(self):
+		return self.partit
+
+#class Classificacio(models.Models):
+	
