2a27053dc6b53d1ccf701e34147cf190cb188090
gjordana <gjordana@gjordana>
2013-04-04 16:29:25 +0200

    ara si , perdo

--- apliWeb/escola/admin.py
+++ apliWeb/escola/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from escola.models import *
 
-admin.site.register(Escola)
-admin.site.register(Instalacions)
+admin.site.register(Escole)
+admin.site.register(Equip)
+admin.site.register(Instalacion)
 admin.site.register(Reglament)
 admin.site.register(Calendari)
-admin.site.register(Junta)
-admin.site.register(Classificacio)
+admin.site.register(Junte)
+admin.site.register(Classificacion)
--- apliWeb/escola/models.py
+++ apliWeb/escola/models.py
@@ -1,50 +1,67 @@
 #encoding:utf-8
 from django.db import models
 from django.contrib.auth.models import User
+from datetime import datetime
 
-class Escola(models.Model):
-	equip = models.CharField('Equip', max_length=200, help_text='Introdueix el nom de Equip')
+class Escole(models.Model):
+	nom = models.CharField('Nom', max_length=20, help_text='Nom Escola')
+	direccio = models.CharField('Direccio', max_length=40, help_text='Direccio de Escola')
+	telf = models.IntegerField('Telefon', max_length=9, help_text='Telefon Escola')
+	
+
+	def __unicode__(self):
+		return self.nom
+
+class Equip(models.Model):
+	categoria = models.CharField('Categoria', max_length=15, help_text='Nom de la categoria')
 	numjug = models.IntegerField('Num de Jugadors', help_text='Nombre de Jugadors')
 	entrenador = models.CharField('Entrenador', max_length=200, help_text='Com es diu l`entrenador')
 	imatge = models.ImageField(upload_to='escola', verbose_name='Imatge')
+	fkEscole = models.ManyToManyField(Escole)
 
 	def __unicode__ (self):
-		return self.equip
+		return self.categoria
 	
 
-class Instalacions(models.Model):
-	vestidor = models.TextField(max_length=200)
-	camp = models.TextField(max_length=200)
-	escola = models.ForeignKey(Escola)
-	
+class Instalacion(models.Model):
+	nom = models.CharField('Nom', max_length=20, help_text='Nom Escola')
+	direccio = models.CharField('Direccio', max_length=40, help_text='Direccio de Escola')
+	fkEscole = models.ForeignKey(Escole)
+
 	def __unicode__(self):
-		return self.vestidor
+		return self.nom
 	
 
 class Reglament(models.Model):
-	bases = models.TextField(max_length=2000)
+	normes = models.TextField(max_length=2000)
+	fkEscole = models.OneToOneField(Escole)
 	
 	def __unicode__(self):
-		return self.bases
+		return self.normes
 
 class Calendari(models.Model):
-	data = models.DateTimeField()
-	partit = models.CharField(max_length=200)
-	
+	jornada = models.IntegerField('Numero Jornada', max_length=2, help_text='Numero de jornada')
+	data = models.DateTimeField(default= datetime.now(), blank=True)
+	#horaconv = models.DateTimeField(default= datetime.now(), blank=True)
+	#horapartit = models.DateTimeField(default= datetime.now(), blank=True)
+	fkEquip = models.ForeignKey(Equip)
 
 	def __unicode__(self):
-		return self.partit
+		return self.jornada
 
-class Junta(models.Model):
-        president = models.CharField('President', max_length=200, help_text='Nom del actual President')
-	vicepresident = models.CharField('Vicepresident', max_length=200, help_text='Nom del Viceprecident')
-	coordinador = models.CharField('Coordinador', max_length=200, help_text='Nom del Coordinador')
-	     
-     	def __unicode__(self):
+class Junte(models.Model):
+	president = models.CharField('President', max_length=200, help_text='Nom del actual President')
+	vicepresident = models.CharField('Vicepresident', max_length=200, help_text='Nom del actual Vicepresident')
+	coordinador = models.CharField('Coordinador', max_length=200, help_text='Nom del actual Coordinador')
+	fkEscole = models.OneToOneField(Escole)
+
+	def __unicode__(self):
  		return self.president
 
-class Classificacio(models.Model):
+class Classificacion(models.Model):
 	possicio = models.IntegerField('Possicio', help_text='Possicio en la classificacio')
-	
+	punts = models.IntegerField('Punts Classificacio', max_length=2)
+	fkEquip = models.OneToOneField(Equip)
+
 	def __unicode__(self):
 		return self.possicio
